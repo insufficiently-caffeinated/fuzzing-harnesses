7a3b4fb9a5aa17a3cb905308eed784922cd76472
Danieil <skrinikovd@gmail.com>
2022-02-16 21:04:01 +0000

    Convert to c

--- libpng/read_fuzzer.c
+++ libpng/read_fuzzer.c
@@ -4,14 +4,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h> 
-#include <vector>
+// #include <vector>
 
 #define PNG_INTERNAL
 #include "png.h"
 
 void* limited_malloc(png_structp, png_alloc_size_t size) {
   if (size > 8000000)
-    return nullptr;
+    return NULL;
   
   void* data = malloc(size);
   caffeine_make_symbolic(data, size, "data2");
@@ -32,26 +32,28 @@ void warn_fn(png_structp ptr, png_const_charp text) {
 
 
 static const int kPngHeaderSize = 8;
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size < kPngHeaderSize) {
     return 0;
   }
 
-  std::vector<unsigned char> v(data, data + size);
-  if (png_sig_cmp(v.data(), 0, kPngHeaderSize)) {
-    // not a PNG.
-    return 0;
-  }
+  // std::vector<unsigned char> v(data, data + size);
+  // void* v = malloc()
+  
+  // if (png_sig_cmp(v.data(), 0, kPngHeaderSize)) {
+  //   // not a PNG.
+  //   return 0;
+  // }
 
   png_structp png_ptr = png_create_read_struct
-    (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
   assert(png_ptr);
 
 #ifdef MEMORY_SANITIZER
   png_set_user_limits(png_ptr, 65535, 65535);
 #endif
-  png_set_mem_fn(png_ptr, nullptr, limited_malloc, default_free);
-  png_set_error_fn(png_ptr, nullptr, error_fn, warn_fn);
+  png_set_mem_fn(png_ptr, NULL, limited_malloc, default_free);
+  png_set_error_fn(png_ptr, NULL, error_fn, warn_fn);
 
   png_set_crc_action(png_ptr, PNG_CRC_QUIET_USE, PNG_CRC_QUIET_USE);
 
@@ -62,9 +64,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  png_set_progressive_read_fn(png_ptr, nullptr, nullptr, nullptr, nullptr);
-  png_process_data(png_ptr, info_ptr, const_cast<uint8_t*>(data), size);
-  png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
+  png_set_progressive_read_fn(png_ptr, NULL, NULL, NULL, NULL);
+  png_process_data(png_ptr, info_ptr, (uint8_t*)(data), size);
+  png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
 
   return 0;
 }
