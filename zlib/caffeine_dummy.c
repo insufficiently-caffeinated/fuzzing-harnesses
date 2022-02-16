14165b804dda93000f96d82a5b97a9321f29a73f
Phantomical <phantom@lynches.ca>
2022-02-16 18:18:01 +0000

    Cleanup unused files

--- zlib/caffeine_dummy.c
+++
@@ -1,8 +0,0 @@
-
-#include <caffeine.h>
-
-// Dummy definitions for caffeine methods
-
-void caffeine_assert(bool cond) {}
-void caffeine_assume(bool cond) {}
-void caffeine_make_symbolic(void *mem, size_t size, const char *name) {}
